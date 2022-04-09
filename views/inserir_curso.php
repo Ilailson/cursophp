ad65e80fe693ada8aa4b53ce088654a2ea995f31
Ilailson <ilailson@hotmail.com>
2022-04-08 21:25:19 -0300

    Conclusão crud basico php

--- views/inserir_curso.php
+++ views/inserir_curso.php
@@ -1,7 +1,11 @@
+<!-- 
+	se não existir a variavel editar
+	execute o codigo abaixo que é uma página de inserção
+-->
+<?php if(!isset($_GET['editar'])){ ?>
+	<!-- Método post é criptografado-->
 
-<!-- formulário de inserção do curso-->
 <h1>Inserir novo curso</h1>
-<!--enviado a informação desse formulário para o processa cursso.php -->
 <form method="post" action="processa_curso.php">
 	<br>
 	<label>Nome curso:</label><br>
@@ -12,3 +16,23 @@
 	<input type="submit" value="Inserir curso">
 </form>
 
+
+<?php } 
+else { ?>
+	<?php while($linha = mysqli_fetch_array($consulta_cursos)){ ?>
+		<?php if($linha['id_curso'] == $_GET['editar']){ ?>
+
+			<h1>Editar curso</h1>
+			<form method="post" action="edita_curso.php">
+				<input type="hidden" name="id_curso" value="<?php echo $linha['id_curso']; ?>">
+				<br>
+				<label>Nome curso:</label><br>
+				<input type="text" name="nome_curso" placeholder="Insira o nome do curso" value="<?php echo $linha['nome_curso']; ?>">
+				<br><br>
+				<label>Carga horária</label><br>
+				<input type="text" name="carga_horaria" placeholder="Insira a carga horária"  value="<?php echo $linha['carga_horaria']; ?>"><br><br>
+				<input type="submit" value="Editar curso">
+			</form>
+		<?php } ?>
+	<?php } ?>
+<?php } ?>
